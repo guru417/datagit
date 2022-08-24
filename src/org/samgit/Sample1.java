8bae60494093faf59d85bf8f00925a4f2342ce11
guru417 <kguru417@gmail.com>
2022-08-24 11:28:08 +0530

    completed

---
+++ src/org/samgit/Sample1.java
@@ -0,0 +1,27 @@
+package org.samgit;
+
+public class Sample1 {
+
+
+public static class Phone {
+		private void Phonename() {
+			System.out.println("my phone name is vivo");
+		}
+		private void Phonemiei() {
+			System.out.println("my phone miei is qwe2345");
+		}
+		private void Phoneos() {
+			System.out.println("my phone os is android");
+		}
+		
+		public static void main(String[] args) {
+			Phone p= new Phone();
+			p.Phonename();
+			p.Phonemiei();
+			p.Phoneos();
+
+		}
+
+	}
+
+}
